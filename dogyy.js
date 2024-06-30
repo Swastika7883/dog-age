9d292207cdbcb57dc47722e6d0d8982f9ce011e4
Swastika7883 <swastikabera2004@gmail.com>
2024-06-30 15:52:05 +0530

    Create dogyy.js
    added changes in files

---
+++ dogyy.js
@@ -0,0 +1,20 @@
+// initializing myage
+let myAge = 19;
+// early age as follows
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+laterYears = myAge -2;
+// to calculate the no. of dog years accounted for by ur later years 
+laterYears *= 4;
+
+console.log(earlyYears , laterYears);
+
+let myAgeInDogYears = (earlyYears + laterYears)
+//console.log(myAgeInDogYears)
+
+//this represents my name in all small letters
+ let myName =('Swastika'.toLowerCase());
+
+ console.log(`My name is ${myName} . I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
+
